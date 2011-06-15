de2b8d6aafbd148f055686359e374e5e3b894ec1
Jake Dahn <admin@jakedahn.com>
2011-06-14 18:02:22 -0700

    adding users to api

---
+++ openstack/extras/users.py
@@ -0,0 +1,35 @@
+from openstack.api import base
+
+
+class User(base.Resource):
+    def __repr__(self):
+        return "<User %s>" % self._info
+
+    def delete(self):
+        self.manager.delete(self)
+
+    def update(self, description=None, enabled=None):
+        description = description or self.description or '(none)'
+        self.manager.update(self.id, description, enabled)
+
+
+class UserManager(base.ManagerWithFind):
+    resource_class = User
+
+    def get(self, user_id):
+        return self._get("/users/%s" % user_id, "user")
+
+    def create(self, user_id, description, enabled=True):
+        params = {"tenant": {"id": user_id,
+                             "email": email,
+                             "enabled": enabled}}
+
+        return self._create('/users', params, "user")
+
+    def list(self):
+        """
+        Get a list of users.
+        :rtype: list of :class:`User`
+        """
+        return self._list("/users", "users")
+
